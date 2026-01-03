12817a4160a6124be3399042a0e733fa82e2a0a0
Adyan <78735036+AdyanMidzic08@users.noreply.github.com>
2026-01-03 20:52:55 +0100

    frontend added + List

--- src/storage.ts
+++ src/storage.ts
@@ -1,6 +1,6 @@
 import { promises as fs } from "fs";
 import path from "path";
-import { Movie } from "./types"
+import { Movie } from "./types";
 
 const FILE_PATH = path.join(__dirname, "..", "movies.json");
 
@@ -9,11 +9,10 @@ export async function loadMovies(): Promise<Movie[]> {
     const data = await fs.readFile(FILE_PATH, "utf-8");
     return JSON.parse(data) as Movie[];
   } catch (err) {
-    console.log("notes.json nicht gefunden oder leer → leeres Array");
     return [];
   }
 }
 
 export async function saveMovies(movies: Movie[]): Promise<void> {
-    await fs.writeFile(FILE_PATH, JSON.stringify(movies, null, 2), "utf-8");
+  await fs.writeFile(FILE_PATH, JSON.stringify(movies, null, 2), "utf-8");
 }
